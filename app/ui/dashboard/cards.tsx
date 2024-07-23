73b34a9c9d58c47c2b93581546ec17690c5209b6
Bardia <Bardiaa830@gmail.com>
2024-07-23 15:48:14 +0330

    implement database and dynamic rendering

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -12,24 +12,28 @@ const iconMap = {
   pending: ClockIcon,
   invoices: InboxIcon,
 };
+import { fetchCardData } from "@/app/lib/data";
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
-
 export function Card({
   title,
   value,
