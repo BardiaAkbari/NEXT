922d6f3487add98bc8c6ccd78b17e66054cf52f2
Bardia <Bardiaa830@gmail.com>
2024-07-29 18:53:45 +0330

    form validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
-import { updateInvoice } from "@/app/lib/actions";
+import { updateInvoice, State } from "@/app/lib/actions";
+import { useActionState } from "react";
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,10 +19,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -34,6 +37,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby="customerId-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -45,6 +49,16 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div
+            id="customerId-error"
+            aria-live="polite"
+            className="mt-2 text-sm text-red-500"
+          >
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
           </div>
         </div>
 
@@ -63,8 +77,19 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div
+              id="amount-error"
+              aria-live="polite"
+              className="mt-2 text-sm text-red-500"
+            >
+              {state.errors?.amount &&
+                state.errors.amount.map((error: string) => (
+                  <p key={error}>{error}</p>
+                ))}
             </div>
           </div>
         </div>
@@ -100,6 +125,7 @@ export default function EditInvoiceForm({
                   value="paid"
                   defaultChecked={invoice.status === "paid"}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="paid"
@@ -111,6 +137,24 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        <div
+          id="status-error"
+          aria-live="polite"
+          className="mt-2 text-sm text-red-500"
+        >
+          {state.errors?.status &&
+            state.errors.status.map((error: string) => (
+              <p key={error}>{error}</p>
+            ))}
+        </div>
+
+        <div
+          id="message-error"
+          aria-live="polite"
+          className="mt-2 text-sm text-red-500"
+        >
+          {state.message && <p key={state.message}>{state.message}</p>}
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
